29291ef249524794946e3306b446b97143527bbb
zsandibe <Sandibekov1998@gmail.com>
2023-06-25 00:01:20 +0600

    upd: fixed path to ui

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -10,7 +10,7 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", c.Page)
 	mux.HandleFunc("/ascii-art", c.Ascii)
-	mux.Handle("/templates/", http.StripPrefix("/templates/", http.FileServer(http.Dir("templates"))))
+	mux.Handle("/ui/", http.StripPrefix("/ui", http.FileServer(http.Dir("./ui"))))
 	log.Println("Запуск веб-сервера на http://localhost:8080/")
 	err := http.ListenAndServe(":8080", mux)
 	log.Println("You can`t ", err)
